72b5f345f3b79c97865047377f91e79a37aeb14f
Nonete <amorenolopezbarajas@pm.me>
2026-07-01 12:28:43 +0000

    chore: update version to v0.1.1 across project files
    - Bumped version number in package.json, pyproject.toml, and docs.ts to v0.1.1.
    - Updated bootstrap.sh script to reflect the new version in the download URL and default branch reference.

--- apps/backend/src/routers/docs.ts
+++ apps/backend/src/routers/docs.ts
@@ -17,7 +17,7 @@ const handler = new OpenAPIHandler(appRouter, {
         servers: [{ url: "/api" }],
         info: {
           title: "Playbook Runner API",
-          version: "v0.1.0",
+          version: "v0.1.1",
           contact: {
             name: "Nonete",
             email: "amorenolopezbarajas@pm.me",
@@ -26,7 +26,7 @@ const handler = new OpenAPIHandler(appRouter, {
           description: "API for the Playbook Runner",
           license: {
             name: "GNU General Public License v3.0",
-            url: "https://raw.githubusercontent.com/Nonetss/playbook-runner/refs/tags/v0.1.0/LICENSE",
+            url: "https://raw.githubusercontent.com/Nonetss/playbook-runner/refs/tags/v0.1.1/LICENSE",
           },
         },
         security: [{ ApiKey: [] }, { BearerAuth: [] }],
